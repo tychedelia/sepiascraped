419b4271a55c12c5b3310ef200a08424600bdd7e
Charlotte McElwain <charlotte.c.mcelwain@gmail.com>
2024-03-24 21:22:57 -0700

    Fix egui.

--- assets/project.scm
+++ assets/project.scm
@@ -2,10 +2,10 @@
 (op! 'noise "noise1")
 (op! 'composite "composite")
 (op! 'cuboid "cuboid1")
-(op! 'window "window")
 (op! 'standard-material "mat")
+(op! 'window "window")
 
-(param! (op "noise1") "Strength" (/ *time* 100))
-(param! (op "myRamp") "Mode" 0)
+(param! (op "noise1") "Strength" (/ *time* 10))
+(param! (op "myRamp") "Mode" 2)
 (param! (op "window") "Texture" (op "mat"))
 (param! (op "mat") "Texture" (op "composite"))
